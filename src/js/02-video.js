1aec291aa125ba5e0053903ef27c492388e51243
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-02-21 20:12:08 +0200

    Done task - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,24 +5,17 @@ const iframe = document.querySelector('iframe');
 const playerRef = new Vimeo.Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
 const currentTime = localStorage.getItem(STORAGE_KEY);
-    
 
-
-function timeUpdate({ seconds }) {
-
-    localStorage.setItem(STORAGE_KEY, seconds);
-player.setCurrentTime(30.456).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+if (currentTime) {
+    playerRef.setCurrentTime(currentTime);
 }
-playerRef.on('timeupdate', throttle(timeUpdate, 1000 ));
+
+function timeUpdate() {
+    playerRef.getCurrentTime().then(function(seconds) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+        
+    }).catch(function(error) {
+        console.log("Error") 
+    });
+}
+playerRef.on('timeupdate', throttle(timeUpdate, 1000));
