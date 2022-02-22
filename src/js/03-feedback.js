ae21eedee81f0f18ee57d1143c5ed9278d1c619d
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-02-22 16:14:27 +0200

    done task - 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,74 +2,44 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
+const form = document.querySelector('.feedback-form');
+// const email = document.querySelector('.feedback-form  input');
+// const message = document.querySelector('.feedback-form  textarea');
+const parcedItem = {};
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('.feedback-form  input'),
-  textarea: document.querySelector('.feedback-form  textarea'),
-};
+initForm();
 
-refs.form.addEventListener('submit', onFormSubmit);
-// refs.email.addEventListener('input', throttle(onEmailInput, 500));
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('change', throttle(changeForm, 500));
 
-populateTextarea();
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  //   console.log('Отправляем форму');
-  evt.currentTarget.reset();
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log('Form sent');
+  const formData = new FormData(form);
+  // console.log(formData);
+  formData.forEach((key, value) => console.log(`${value}-${key}`));
+  event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  // console.log(event.currentTarget);
 }
+function changeForm(event) {
+  // let parcedItem = localStorage.getItem(STORAGE_KEY);
+  // parcedItem = parcedItem ? JSON.parse(parcedItem) : {};
+  parcedItem[event.target.name] = event.target.value;
+  // console.log(parcedItem);
 
-function onTextareaInput(evt) {
-  const message = evt.target.value;
-  // console.log(message);
-  localStorage.setItem(STORAGE_KEY, message);
+  if (!parcedItem) return;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(parcedItem));
 }
+function initForm() {
+  let persistedItem = localStorage.getItem(STORAGE_KEY);
+  // console.log(parcedItem);
 
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (persistedItem) {
+    persistedItem = JSON.parse(persistedItem);
+    console.log(persistedItem);
 
-  if (savedMessage) {
-    refs.textarea.value = savedMessage;
+    Object.entries(persistedItem).forEach(([key, value]) => (form.elements[key].value = value));
   }
 }
-
-// // Домой
-// // сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
-
-refs.form.addEventListener('input', e => {
-  // console.log(e.target.name);
-  // console.log(e.target.value);
-
-  formData[e.target.name] = e.target.value;
-  // console.log(formData);
-
-  const formDataJSON = JSON.stringify(formData);
-
-  localStorage.setItem(STORAGE_KEY, formDataJSON);
-  // console.log(formDataJSON);
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  console.log('savedData', savedData);
-
-  const parsedData = JSON.parse(formDataJSON);
-  console.log('parsedData', parsedData);
-});
-
-// function onEmailInput(evt) {
-//   const email = evt.target.value;
-//   console.log(email);
-
-//   localStorage.setItem(STORAGE_KEY, email);
-// }
-// populateEmail();
-
-// function populateEmail() {
-//   const savedEmail = localStorage.getItem(STORAGE_KEY);
-
-//   if (savedEmail) {
-//     refs.email.value = savedEmail;
-//   }
-// }
